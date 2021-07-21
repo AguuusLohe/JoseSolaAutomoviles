250df435f99a78cc8645dafa71aa4b9a6e5d393f
AguuusLohe <86483151+AguuusLohe@users.noreply.github.com>
2021-07-21 18:23:48 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,14 @@
+<?php
+$myemail = 'aguslohe1@gmail.com';
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$to = $myemail;
+$email_subject = "Nuevo mensaje: $subject";
+$email_body = "Haz recibido un nuevo mensaje. \n Nombre: $name \n Correo: $email \n Mnesaje: \n $message";
+$headers = "From: $email";
+
+mail($to, $email_subject, $email_body, $headers);
+echo "El mensaje se ha enviado correctamente";
+?>
